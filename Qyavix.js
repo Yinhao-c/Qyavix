7bbc61b9e9200ba1a7e0c38022040ad01d8db4a6
Yinhao Chen <143989008+Yinhao-c@users.noreply.github.com>
2025-08-27 08:00:48 -0400

    Add files via upload

---
+++ Qyavix.js
@@ -0,0 +1,10 @@
+let Qyavix = [];
+
+export const u = i => (
+  Qyavix[0] = Qyavix[0] ?? i,
+  [Qyavix[0], v => (Qyavix[0] = typeof v === 'function' ? v(Qyavix[0]) : v, Qyavix[1]())]
+);
+
+export const r = (C, R) => (
+  (Qyavix[1] = () => (R.innerHTML = '', R.appendChild(C()), R.dataset.framework='Qyavix')), Qyavix[1]()
+);
